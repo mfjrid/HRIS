c545b89d490d01638453f12101f2a49a6de0f0da
Fajar <mfjr48@gmail.com>
2023-07-03 14:49:25 +0700

    Add Team Factory

---
+++ database/factories/TeamFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Team>
+ */
+class TeamFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->jobTitle(),
+            'logo' => $this->faker->imageUrl(),
+            'company_id' => $this->faker->numberBetween(1, 10),
+        ];
+    }
+}
