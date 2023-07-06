c6fb2693cccc7233ebd45309d1a668742f4fc0ea
Fajar <mfjr48@gmail.com>
2023-07-06 16:30:39 +0700

    Update API Company Controller

--- app/Http/Controllers/API/CompanyController.php
+++ app/Http/Controllers/API/CompanyController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Company;
 use Illuminate\Http\Request;
 
 class CompanyController extends Controller
 {
-    //
+    public function all(Request $request)
+    {
+        $id = $request->input('id');
+        $limit = $request->input('limit', 10);
+
+        if ($id) {
+            $company = Company::with(['users'])->find($id);
+        }
+    }
 }
